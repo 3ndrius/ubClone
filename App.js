b574a80d260610b403ac083c7622c9a8499dd268
3ndrius <you@example.com>
2019-09-20 17:25:38 +0200

    Adding map and track current location

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
   View,
   Text,
   StatusBar,
+  Dimensions
  
 } from 'react-native';
 
@@ -25,23 +26,105 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import Geolocation from '@react-native-community/geolocation';
 
+const { width, height } = Dimensions.get('window');
+
+const SCREEN_HEIGHT = height;
+const SCREEN_WIDTH = width;
+const ASPECT_RATIO = width/height;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 class App extends Component {
+
+
+  state = {
+    initialPosition: {
+        latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+    },
+    markerPosition:{
+      latitude: 0,
+      longitude: 0
+    }
+  }
+
+  watchID: ?number = null
+
+  componentDidMount() {
+   
+    Geolocation.getCurrentPosition((position) => {
+      let lat = parseFloat(position.coords.latitude);
+      let long = parseFloat(position.coords.longitude);
+
+      let initialRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+
+      this.setState({
+        initialPosition: initialRegion,
+        markerPosition: initialRegion
+      })
+    },
+    (error) => alert(JSON.stringify(error)),
+    {enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 })
+
+    this.watchID = Geolocation.watchPosition((position ) => {
+      let lat = parseFloat(position.coords.latitude);
+      let long = parseFloat(position.coords.longitude);
+
+      let lastRegion = {
+        latitude: lat,
+        longitude: long,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA
+      }
+      this.setState({
+        initialPosition: lastRegion,
+        markerPosition: lastRegion
+      })
+    })
+
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID)
+  }
+
+
 
   render() {
     return (
       <View style={styles.container}>
+        <View>
+          <Text>Maps</Text>
+        </View>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}
-      ></MapView>
+        region={this.state.initialPosition}
+        // region={{
+        //   latitude: 37.78825,
+        //   longitude: -122.4324,
+        //   latitudeDelta: 0.015,
+        //   longitudeDelta: 0.0121,
+        // }}
+      >
+        <MapView.Marker 
+        coordinate={this.state.markerPosition}>
+          <View style={styles.radius}>
+            <View style={styles.marker}>
+
+            </View>
+          </View>
+        </MapView.Marker>
+      </MapView>
     </View>
     )
   }
@@ -49,8 +132,12 @@ class App extends Component {
 
 
 const styles = StyleSheet.create({
-  container: { ... StyleSheet.absoluteFillObject },
-  map: { ...StyleSheet.absoluteFillObject }
+  container: { 
+    flex: 1
+   },
+  map: { 
+    flex: 1
+   }
 });
 
 export default App;
